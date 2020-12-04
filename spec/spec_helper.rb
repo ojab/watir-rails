35cf35d057351803ad2d19ac0eacb29546613fdf
ojab <ojab@ojab.ru>
2020-12-04 13:01:03 +0000

    Add CI with GitHub Actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
 require "simplecov"
-require 'coveralls'
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+if ENV["LCOV_REPORT_PATH"]
+  require "simplecov-lcov"
+
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = ENV.fetch('LCOV_REPORT_PATH')
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+end
+
 SimpleCov.start
 
 # Make sure that fake watir gems are loaded for specs.
